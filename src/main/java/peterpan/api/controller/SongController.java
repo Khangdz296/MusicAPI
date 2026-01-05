223093e24453d8aaa73c86df406e2a0694b7efef
Duy Hoang <23162025@student.hcmute.edu.vn>
2026-01-05 20:51:09 +0700

    them view cho song

--- src/main/java/peterpan/api/controller/SongController.java
+++ src/main/java/peterpan/api/controller/SongController.java
@@ -1,6 +1,7 @@
 package peterpan.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import peterpan.api.model.Song;
 import peterpan.api.repository.SongRepository;
@@ -26,9 +27,23 @@ public class SongController {
         return songRepository.findByTitleContainingIgnoreCaseOrArtistContainingIgnoreCase(keyword, keyword);
     }
 
-    // API 3: Lấy nhạc mới cập nhật 7 ngày
+
+//    // API 3: Lấy nhạc mới cập nhật 7 ngày
+//    @GetMapping("/new-updated")
+//    public List<Song> getNewSongs() {
+//        return songRepository.findNewSongs7Days();
+//    }
+
     @GetMapping("/new-updated")
     public List<Song> getNewSongs() {
-        return songRepository.findNewSongs7Days();
+        return songRepository.findTop5ByOrderByIdDesc();
     }
+
+    // THÊM MỚI: API 4 - Lấy BXH Top Views
+    @GetMapping("/top-views")
+    public ResponseEntity<List<Song>> getTopSongs() {
+        List<Song> topSongs = songRepository.findTop10ByOrderByViewsDesc();
+        return ResponseEntity.ok(topSongs);
+    }
+
 }
